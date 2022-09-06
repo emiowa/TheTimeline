612418d2ab34ed6fe3a3b41d4fc98ad33e6b19e3
Emi Owashi Vallejo <emiowa@hotmail.com>
2022-09-06 22:14:06 +0900

    Copiar archivo js del main

---
+++ index.js
@@ -0,0 +1,17 @@
+let newDiv = document.querySelector(".timeline");
+newDiv.addEventListener("load", createTimeline(3));
+
+function createTimeline(number){
+    let year = new Date().getFullYear() - number;
+    for(let i = 0; i < 5; i++){
+        year = year + 1;
+        addYears(year);
+    }
+}
+
+function addYears(yearsToAdd){
+    let newP = document.createElement("p");
+    let newText = document.createTextNode(yearsToAdd);
+    newP.append(newText);
+    newDiv.append(newP);
+}
